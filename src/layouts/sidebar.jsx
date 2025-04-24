68df7ff9b4a80bf9c411f4d9aed4f1562422ae02
Joshua Gocotano <gocotano.joshua02@gmail.com>
2025-04-24 15:13:12 +0800

    progress progress

--- src/layouts/sidebar.jsx
+++ src/layouts/sidebar.jsx
@@ -1,12 +1,25 @@
 import { forwardRef } from "react";
 
+import opascorlogo from "@/assets/opascorlogo2.png";
+
+import { cn } from "@/utils/cn";
+
 export const Sidebar = forwardRef(({}, ref) => {
     return (
         <aside
             ref={ref}
-            className="fixed z-[100] flex h-full w-[240px] flex-col overflow-x-hidden border-r border-slate-300 bg-white [transition:_width_300ms_cubic-bezier(0.4,_0,_0.2,_1),_left_300ms_cubic-bezier(0.4,_0,_0.2,_1),_background-color_150ms_cubic-bezier(0.4,_0,_0.2,_1),_border_150ms_cubic-bezier(0.4,_0,_0.2,_1)] dark:border-slate-700 dark:bg-slate-900"
+            className={cn(
+                "[transition:_width_300ms_cubic-bezier(0.4,_0,_0.2,_1),_left_300ms_cubic-bezier(0.4,_0,_0.2,_1),_background-color_150ms_cubic-bezier(0.4,_0,_0.2,_1),_border_150ms_cub ic-bezier(0.4,_0,_0.2,_1)] fixed z-[100] flex h-full w-[240px] flex-col overflow-x-hidden border-r border-slate-300 bg-white dark:border-slate-700 dark:bg-slate-900",
+            )}
         >
-            Sidebar
+            <div className="flex gap-x-3 p-3">
+                <img
+                    src={opascorlogo}
+                    alt="Logo"
+                    className="h-10 w-10"
+                />
+                <h1 className="text-xl font-bold text-slate-900 dark:text-slate-200">OPASCOR</h1>
+            </div>
         </aside>
     );
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class",
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Poppins", "sans-serif"],
+            },
+        },
     },
 };
