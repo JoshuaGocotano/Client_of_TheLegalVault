5dbac59735e8ebcdae84334a3a623f00ea739139
Joshua <gocotano.joshua02@gmail.com>
2025-11-26 13:15:18 +0800

    ... profile modal

--- src/components/profile-modal.jsx
+++ src/components/profile-modal.jsx
@@ -92,11 +92,12 @@ export const ProfileModal = ({ onClose }) => {
                     body: form,
                 });
             } else {
+                const formDataWithUpdater = { ...formData, user_last_updated_by: user.user_id };
                 response = await fetch(`http://localhost:3000/api/users/${user.user_id}`, {
                     method: "PUT",
                     credentials: "include",
                     headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(formData),
+                    body: JSON.stringify(formDataWithUpdater),
                 });
             }
 
